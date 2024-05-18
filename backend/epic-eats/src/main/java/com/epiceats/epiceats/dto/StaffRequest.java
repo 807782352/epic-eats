4ecf2e9865ce96cee7834f6eebfc57d144efaf3e
z297xu <kyrie807782352@gmail.com>
2024-05-18 13:20:44 -0400

    + addStaff

---
+++ backend/epic-eats/src/main/java/com/epiceats/epiceats/dto/StaffRequest.java
@@ -0,0 +1,11 @@
+package com.epiceats.epiceats.dto;
+
+public record StaffRequest(
+        String firstName,
+        String lastName,
+        String password,
+        String email,
+        String phone,
+        Integer role
+) {
+}
